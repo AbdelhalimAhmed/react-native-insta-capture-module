d48c3e1f818703ea7ba010490657cb70ac66795d
abdelhalim <abdelhalim.ahmed95@gmail.com>
2018-11-24 19:08:54 +0200

    using instacapture modul into react-native-module

--- android/src/main/java/com/reactlibrary/RNInstaCaptureModuleModule.java
+++ android/src/main/java/com/reactlibrary/RNInstaCaptureModuleModule.java
@@ -5,75 +5,63 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.WritableArray;
-import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReadableMap;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.util.Base64;
+
+import com.tarek360.instacapture.Instacapture;
+import com.tarek360.instacapture.listener.SimpleScreenCapturingListener;
+
+import java.io.ByteArrayOutputStream;
+
+import rx.functions.Action1;
 
 public class RNInstaCaptureModuleModule extends ReactContextBaseJavaModule {
+    private final ReactApplicationContext reactContext;
 
-  private final ReactApplicationContext reactContext;
-
-  public RNInstaCaptureModuleModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
-
-  @Override
-  public String getName() {
-    return "RNInstaCaptureModule";
-  }
-
-  @ReactMethod //to expose a method to JavaScript
-  public void getModuleList(final Callback callback) {
-    try {
-        WritableArray list = getList();
-        callback.invoke(null, list);
-    } catch (Exception e) {
-        callback.invoke("Error", null);
+    public RNInstaCaptureModuleModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
     }
-  }
+
+    @Override
+    public String getName() {
+        return "RNInstaCaptureModule";
+    }
 
 
-  private WritableArray getList() {
-    WritableArray listArr = Arguments.createArray();
-    listArr.pushString("Halim is here");
-    listArr.pushString("react-native-camera");
-    listArr.pushString("react-native-map");
+    @ReactMethod
+    public void takeScreenshot(ReadableMap config, final Callback successCallback, final Callback cancelCallback) {
+        Activity currentActivity = getCurrentActivity();
 
-    return listArr;
-  }
+        Instacapture.capture(currentActivity, new SimpleScreenCapturingListener() {
+            @Override
+            public void onCaptureComplete(Bitmap bitmap) {
+                try {
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                    byte[] b = baos.toByteArray();
+                    String temp = Base64.encodeToString(b, Base64.DEFAULT);
 
-  protected void showAlertDialog() {
-    AlertDialogFragment.newInstance(R.string.dialog_title, R.string.dialog_message)
-            .show(getSupportFragmentManager(), "dialogFragment");
-}
+                    successCallback.invoke(temp);
 
-protected void captureScreenshot(@Nullable View... ignoredViews) {
+                } catch (Exception e) {
+                    cancelCallback.invoke(e);
+                }
 
-    Instacapture.capture(this, new SimpleScreenCapturingListener() {
-        @Override
-        public void onCaptureComplete(Bitmap bitmap) {
-
-            Utility.getScreenshotFileObservable(BaseSampleActivity.this, bitmap)
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(new Action1<File>() {
-                        @Override
-                        public void call(File file) {
-
-                            startActivity(ShowScreenShotActivity.buildIntent(BaseSampleActivity.this,
-                                    file.getAbsolutePath()));
-                        }
-                    });
-        }
-    }, ignoredViews);
+            }
+        }, null);
 
 
-    Instacapture.captureRx(this, ignoredViews).subscribe(new Action1<Bitmap>() {
-        @Override
-        public void call(Bitmap bitmap) {
+        Instacapture.captureRx(currentActivity, null).subscribe(new Action1<Bitmap>() {
+            @Override
+            public void call(Bitmap bitmap) {
 
-        }
-    });
+            }
+        });
 
-}
+
+    }
+
 }
